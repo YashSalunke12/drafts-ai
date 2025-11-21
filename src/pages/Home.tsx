44b82bb295ac7413bf230a86d35947c8bfdded67
Yash Salunke <salunkeyaash12@gmail.com>
2025-11-21 18:54:25 +0530

    homepage added

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import BlogList from "../components/BlogList";
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <BlogList />
+    </div>
+  );
+};
+
+export default Home;
